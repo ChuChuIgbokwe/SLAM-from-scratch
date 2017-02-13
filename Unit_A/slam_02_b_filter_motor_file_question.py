d817d412e8440fce44b90c98e6f02489a52381b7
Chu-Chu Igbokwe <chukwunyereigbokwe2015@u.northwestern.edu>
2017-02-12 23:24:58 -0600

    Unit A done, robot width=173.0

--- Unit_A/slam_02_b_filter_motor_file_question.py
+++ Unit_A/slam_02_b_filter_motor_file_question.py
@@ -8,7 +8,7 @@
 # Claus Brenner, 09 NOV 2012
 from math import sin, cos, pi
 from lego_robot import *
-# from pylab import *
+from pylab import *
 
 # This function takes the old (x, y, heading) pose and the motor ticks
 # (ticks_left, ticks_right) and returns the new (x, y, heading).
@@ -30,9 +30,43 @@ def filter_step(old_pose, motor_ticks, ticks_to_mm, robot_width,
 
     else:
         # Turn. Compute alpha, R, etc.
+        # old_theta = old_pose[2]
+        # old_x = old_pose[0] - cos(old_theta) * scanner_displacement
+        # old_y = old_pose[1] - sin(old_theta) * scanner_displacement
+        #
+        # l = motor_ticks[0] * ticks_to_mm
+        # r = motor_ticks[1] * ticks_to_mm
+        # alpha = (r - l) / robot_width
+        # R = l / alpha
+        # theta = (old_theta + alpha) % (2.0 * pi)
+        #
+        # # --->>> Modify your previous implementation.
+        # # First modify the the old pose to get the center (because the
+        # #   old pose is the LiDAR's pose, not the robot's center pose).
+        # # Second, execute your old code, which implements the motion model
+        # #   for the center of the robot.
+        #
+        # Cx = old_x - (R + robot_width / 2.0) * sin(old_theta)
+        # Cy = old_y - (R + robot_width / 2.0) * (-cos(old_theta))
+        #
+        # x = Cx + (R + robot_width / 2.0) * sin(theta)
+        # y = Cy + (R + robot_width / 2.0) * (-cos(theta))
+        #
+        # # Third, modify the result to get back the LiDAR pose from
+        # #   your computed center. This is the value you have to return.
+        # x += cos(theta) * scanner_displacement
+        # y += sin(theta) * scanner_displacement
+        #
+        # return (x, y, theta)
+
+        # Turn. Compute alpha, R, etc.
+        # Modification: subtract offset to compute center.
+        old_x = old_pose[0]
+        old_y = old_pose[1]
         old_theta = old_pose[2]
-        old_x = old_pose[0] - cos(old_theta) * scanner_displacement
-        old_y = old_pose[1] - sin(old_theta) * scanner_displacement
+
+        old_x -= cos(old_theta) * scanner_displacement
+        old_y -= sin(old_theta) * scanner_displacement
 
         l = motor_ticks[0] * ticks_to_mm
         r = motor_ticks[1] * ticks_to_mm
@@ -40,22 +74,21 @@ def filter_step(old_pose, motor_ticks, ticks_to_mm, robot_width,
         R = l / alpha
         theta = (old_theta + alpha) % (2.0 * pi)
 
+        c_x = old_x - (R + robot_width / 2.0) * sin(old_theta)
+        c_y = old_y - (R + robot_width / 2.0) * (-cos(old_theta))
+
+        x = c_x + (R + robot_width / 2.0) * sin(theta)
+        y = c_y + (R + robot_width / 2.0) * (-cos(theta))
+
+        x += cos(theta) * scanner_displacement
+        y += sin(theta) * scanner_displacement
         # --->>> Modify your previous implementation.
         # First modify the the old pose to get the center (because the
         #   old pose is the LiDAR's pose, not the robot's center pose).
         # Second, execute your old code, which implements the motion model
         #   for the center of the robot.
-
-        Cx = old_x - (R + robot_width / 2.0) * sin(old_theta)
-        Cy = old_y - (R + robot_width / 2.0) * (-cos(old_theta))
-
-        x = Cx + (R + robot_width / 2.0) * sin(theta)
-        y = Cy + (R + robot_width / 2.0) * (-cos(theta))
-
         # Third, modify the result to get back the LiDAR pose from
         #   your computed center. This is the value you have to return.
-        x += cos(theta) * scanner_displacement
-        y += sin(theta) * scanner_displacement
 
         return (x, y, theta)
 
@@ -71,7 +104,7 @@ if __name__ == '__main__':
     ticks_to_mm = 0.349
 
     # Measured width of the robot (wheel gauge), in mm.
-    robot_width = 150.0 #173.0
+    robot_width =  173.0 #150.0
 
     # Measured start position.
     pose = (1850.0, 1897.0, 213.0 / 180.0 * pi)
@@ -88,10 +121,10 @@ if __name__ == '__main__':
         filtered.append(pose)
 
 	 # Draw result.
-    # for pose in filtered:
-    #     print pose
-    #     plot([p[0] for p in filtered], [p[1] for p in filtered], 'bo')
-    # show()
+    for pose in filtered:
+        print pose
+    plot([p[0] for p in filtered], [p[1] for p in filtered], 'bo')
+    show()
 
     # Write all filtered positions to file.
     f = open("poses_from_ticks.txt", "w")
